c3b10e50b348a35027e4dfebc19c28c3ce6ffb96
Pgullin <petter.gullin@gmail.com>
2021-02-21 18:46:10 +0100

    fixed a problem with rendering of component when count state changes

--- src/view/uppgift3/Uppgift3.js
+++ src/view/uppgift3/Uppgift3.js
@@ -1,43 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import StarwarsService from '../../shared/api/service/StarwarsService'
 
 
 export const Uppgift3 = () => {
 
-	useEffect(() => {
-		getCharacterNameFromStarwarsAPI()
-		
-	})
-
 	const [character, setCharacter] = useState([])
 	const [count, setCount] = useState(1)
 
-	const getCharacterNameFromStarwarsAPI = async () => {
-		const { data } = await StarwarsService.getStarwarsCharacter(count)
-		setCharacter(data)
+	useEffect(() => {
+		const getCharacterNameFromStarwarsAPI = async () => {
+			const { data } = await StarwarsService.getStarwarsCharacter(count)
+			setCharacter(data)
+		}
+		getCharacterNameFromStarwarsAPI()
+	}, [count])
+
+	const getPreviousCharacter = () => {
+		if (count > 1) {
+			const decrementedValue = count - 1
+			setCount(decrementedValue)
+		}
 	}
 
-	const getPreviousCharacter = () =>{
-if(count >1){
-	setCount(count-1)
-	displayCharacterName()
-}
+	const getNextCharacter = () => {
+		const incrementedValue = count + 1
+		setCount(incrementedValue)
 	}
-
-	const getNextCharacter = () =>{		
-			setCount(count + 1 )
-			displayCharacterName()		
-			}			
 
 	const buttons = () => {
 		return <div>
-			<button onClick={()=>getNextCharacter()}>Increment</button> <br />
-			<button onClick={()=>getPreviousCharacter()}>Decrement</button>
+			<button onClick={() => getNextCharacter()}>Increment</button> <br />
+			<button onClick={() => getPreviousCharacter()}>Decrement</button>
 		</div>
 	}
 
 	const displayCharacterName = () => {
-	
+
 		if (character || undefined) {
 			return <div>
 				<h2>{(character || undefined)?.name}</h2>
@@ -62,21 +60,21 @@ Denna vy skall använda sig av useEffect tillsammans med useState för att hämt
 	1. Du hittar två variabler i denna vy, character & count, dessa variabler skall bytas ut mot varsitt state.
 	(variabelnamnen skall fortsatt heta likadant och de tidigare variabeldeklarationerna kan tas bort)
 
-	2. ditt count state skall ha initialvärdet 1 
+	2. ditt count state skall ha initialvärdet 1
 	och ditt character initialvärde skall vara en tom array
 
-	3. i funktionen getCharacterNameFromStarwarsAPI() skall du spara 
+	3. i funktionen getCharacterNameFromStarwarsAPI() skall du spara
 	värdet av data variabeln i ditt character state
 
-	4. I funktionen buttons() hittar du två button taggar. Skriv funktionalitet för knapparna att 
+	4. I funktionen buttons() hittar du två button taggar. Skriv funktionalitet för knapparna att
 	addera värdet av count med 1 respektive subtrahera värdet med 1
 
-	6. Vid första renderingen av denna vy-komponent 
+	6. Vid första renderingen av denna vy-komponent
 	skall funktionen getCharacterNameFromStarwarsAPI() att anropas
 
 	7. När värdet av count uppdateras skall det ske en ny rendering
 
-	Ifall du lyckats slutföra detta ordentligt skall du nu i applikationen kunna hämta ett nytt karaktärnamn 
+	Ifall du lyckats slutföra detta ordentligt skall du nu i applikationen kunna hämta ett nytt karaktärnamn
 	genom att använda dig av de knappar som finns innuti buttons() funktionen.
 
  */
